c9fee7365ed9f4c6e900e820494e84d375d8ef1b
NicolasV99 <legolasnicolas99@gmail.com>
2024-03-16 12:08:20 -0500

    create substract function

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -3,9 +3,36 @@
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
 
+function add (number1, number2){
+    return number1 + number2;
+}
+
+function addNumbers(addNumber1, addNumber2){
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+
+    document.querySelector('#sum').value = add(addNumber1, addNumber2);
+    document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+}
+
+
+
 
 /* Function Expression - Subtract Numbers */
 
+function subtract(number1, number2){
+    return number1 - number2;
+}
+
+function subtractNumbers(subtractNumber1, subtractNumber2){
+    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
+
+    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+    document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
+    
+
+}
 
 /* Arrow Function - Multiply Numbers */
 
