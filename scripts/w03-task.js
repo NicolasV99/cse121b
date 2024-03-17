b23c7f27db439ef18a1b03d2bd3fe9e6ea0926e7
NicolasV99 <legolasnicolas99@gmail.com>
2024-03-16 19:44:05 -0500

    created product, divide and structure parts

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -15,32 +15,63 @@ function addNumbers(addNumber1, addNumber2){
     document.querySelector('#addNumbers').addEventListener('click', addNumbers);
 }
 
-
-
-
 /* Function Expression - Subtract Numbers */
 
-function subtract(number1, number2){
+const subtract = function(number1, number2){
     return number1 - number2;
 }
 
-function subtractNumbers(subtractNumber1, subtractNumber2){
+const subtractNumbers = function(subtractNumber1, subtractNumber2){
     let subtractNumber1 = Number(document.querySelector('#subtract1').value);
     let subtractNumber2 = Number(document.querySelector('#subtract2').value);
 
     document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
     document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
-    
-
 }
 
 /* Arrow Function - Multiply Numbers */
 
+const multiply = (number1, number2) => number1 * number2;
+
+const multiplyNumbers = (factor1, factor2) => {
+    let factor1 = Number(document.querySelector('#factor1').value);
+    let factor2 = Number(document.querySelector('#factor2').value);
+
+    document.querySelector('#product').value = multiply(factor1, factor2);
+    document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+
+}
 
 /* Open Function Use - Divide Numbers */
 
+function divide(number1, number2){
+    return number1 / number2;
+}
+
+function divideNumbers(dividend, divisor){
+    let dividend = Number(document.querySelector('#dividend').value);
+    let dividend = Number(document.querySelector('#divisor').value);
+
+    document.querySelector('#quotient').value = divide(dividend, divisor);
+    document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
+}
 
 /* Decision Structure */
+
+document.getElementById('#getTotal').addEventListener('click', function(){
+
+    let subtotal = parseFloat(document.getElementById('#subtotal').value);
+    let isMember = document.getElementById('#member').ariaChecked;
+
+    if(isMember){
+        subtotal *= 0.8;
+    }
+
+    let totalSpan = document.getElementById('#total');
+    totalSpan.textContent = `$ ${subtotal.toFixed(2)}`;
+
+});
+
 
 
 /* ARRAY METHODS - Functional Programming */
